b03acbfb4db973675fbb1488390b650119576f29
Akshit Sharma <waprajbhasha9@gmail.com>
2020-09-15 09:47:31 +0530

    Add files via upload

---
+++ TimerController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimerController : MonoBehaviour
+{
+    public int time;
+    public Text TimerText;
+
+
+    private void Start()
+    {
+        StartCoroutine(Countdown());
+    }
+    IEnumerator Countdown()
+    {
+        while (time > 0)
+        {
+            TimerText.text = time.ToString();
+            yield return new WaitForSeconds(1f);
+            time--;
+        }
+    }
+}
